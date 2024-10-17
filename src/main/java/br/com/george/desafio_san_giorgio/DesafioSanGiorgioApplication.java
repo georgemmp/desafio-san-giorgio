8b9453f08ca582faf1a5e5977519b6065f275563
George Michel Machado Pereira <george_pereira@sicredi.com.br>
2024-10-17 14:17:18 -0300

    first commit

---
+++ src/main/java/br/com/george/desafio_san_giorgio/DesafioSanGiorgioApplication.java
@@ -0,0 +1,13 @@
+package br.com.george.desafio_san_giorgio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSanGiorgioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSanGiorgioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/george/desafio_san_giorgio/DesafioSanGiorgioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.george.desafio_san_giorgio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSanGiorgioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
